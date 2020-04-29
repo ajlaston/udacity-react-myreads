573efb55023a3f5a4c4becc156e73a30519abb2c
Adam Laston <30796595+ajlaston@users.noreply.github.com>
2020-04-29 12:43:53 -0700

    Update Search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -7,16 +7,44 @@ import * as BooksAPI from '../BooksAPI';
    	state = {
     	value : '',
       	library : [],
-      	render : false
+      	render : false,
+      	shelf : ''
     }
 
 	updateLib = (event, book) => {
     	BooksAPI.update(book, event.target.value)
-      		.then(this.setState({render : 'update'}))
+      		.then(this.setState({render : 'update'}));
     }
    
    	handleChange = (event) => {
     	this.setState({value : event.target.value, render : 'search'});
+    }
+
+	loop = (book) => {
+      	return  new Promise((resolve,reject)=>{
+          	
+          	BooksAPI.getAll()
+          	.then(lib=>{
+            	for(let i = 0; i < lib.length; ++i){
+                	if(book.title === lib[i].title){
+                      	resolve(lib[i].shelf)
+                    }
+                  
+                  	if(i === lib.length - 1){
+                    	resolve('none');
+                    }
+                }
+            })
+          	
+        })
+        
+        
+    }
+
+	setShelf = (book, library) => {
+      	if(library.indexOf(book) !== library.length-1){
+    	this.loop(book).then(res=>this.setState({shelf : res}))
+        }
     }
 
 	componentDidUpdate(){
@@ -25,8 +53,8 @@ import * as BooksAPI from '../BooksAPI';
 		const {load} = this.props;
 
 		if(render === 'update'){
-        	load();
-        }
+        	load(); this.setState({render : false});
+		}
 
     	return value !== '' && render === 'search' ? BooksAPI.search(value)
 			.then(library=> library.length > 0 ?
@@ -37,7 +65,8 @@ import * as BooksAPI from '../BooksAPI';
   	render(){
       
       	const {library} = this.state;
-
+	
+		
     	return(
         	  <div className="search-books">
             <div className="search-books-bar">
@@ -51,14 +80,14 @@ import * as BooksAPI from '../BooksAPI';
             <div className="search-books-results">
               <ol className="books-grid">
 				{library && library.length > 0 ? 
-                	library.map(book=>( <li key={book.id}>
+                	library.map(book=>( <li key={book.id}> {}
                         <div className="book">
                           <div className="book-top">
                             <div className="book-cover" style={{ width: 128, height: 193, 
                             backgroundImage: book.imageLinks === undefined ? null : `url(${book.imageLinks.smallThumbnail})` }}>											
 							</div>
                             <div className="book-shelf-changer">
-                              <select value='none' onChange={(event)=>this.updateLib(event, book )} >
+                              <select value={this.state.shelf} onClick={()=>this.setShelf(book,library)} onChange={(event)=>this.updateLib(event, book )} >
                                 <option value="move" disabled>Move to...</option>
                                 <option value="currentlyReading">Currently Reading</option>
                                 <option value="wantToRead">Want to Read</option>
